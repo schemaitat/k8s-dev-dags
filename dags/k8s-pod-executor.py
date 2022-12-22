6fe33b191f99a6d668f002ff0d647aa3bcd57095
André Schemaitat <a.schemaitat@gmail.com>
2022-12-22 11:58:23 +0100

    update

--- dags/k8s-pod-executor.py
+++ dags/k8s-pod-executor.py
@@ -2,19 +2,11 @@ import datetime
 import pendulum
 from airflow import models
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 from kubernetes.client import models as k8s_models
-
-# A Secret is an object that contains a small amount of sensitive data such as
-# a password, a token, or a key. Such information might otherwise be put in a
-# Pod specification or in an image; putting it in a Secret object allows for
-# more control over how it is used, and reduces the risk of accidental
-# exposure.
-
-
 
 default_args = {
     'owner': 'airflow',
@@ -23,12 +15,12 @@ default_args = {
 with DAG(
         dag_id="k8s-pod",
         default_args=default_args,
-        schedule_interval=None,
+        schedule=None,
         tags=["ex"],
         start_date=pendulum.datetime(2015, 12, 1)
     ) as dag:
 
-    start = DummyOperator(task_id='run_this_first', dag=dag)
+    start = EmptyOperator(task_id='run_this_first', dag=dag)
 
     passing = KubernetesPodOperator(namespace='airflow',
                             image="python:3.6",
